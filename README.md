0c1f0dfbbfb49c592e9e192744ddcb769e364281
Kevin Barabash <kevinb7@gmail.com>
2023-10-21 22:38:13 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# escalier-next
+
+This is a rewrite of [Escalier](http://github.com/escalier-lang/escalier) in
+F#. The hope is the F# will make it easier to develop Escalier.
---
+++ src/Escalier.Parser/Library.fs
@@ -0,0 +1,19 @@
+﻿namespace Escalier.Parser
+
+module Say =
+    let hello name = printfn "Hello %s" name
+
+    open Escalier.Data.Type
+
+    let t =
+        { kind = TypeKind.TypeVar(id = 0, instance = None, bound = None)
+          provenance = ref None }
+
+    open Escalier.Data.Syntax
+
+    let expr: Expr =
+        { kind = Identifer(span = { start = 0; stop = 5 }, name = "foo")
+          span = { start = 0; stop = 5 }
+          inferred_type = ref (Some(t)) }
+
+    t.provenance.Value <- Some(Provenance.Expr(expr))

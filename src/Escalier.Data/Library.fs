41f53aab2ce048ce170623b58835e2bdda3194a7
Kevin Barabash <kevinb7@gmail.com>
2023-10-28 14:16:20 -0400

    handle more operators, add support for indentifiers, strings, and template strings

--- src/Escalier.Data/Library.fs
+++ src/Escalier.Data/Library.fs
@@ -1,311 +1,308 @@
 ﻿namespace rec Escalier.Data
 
 module Syntax =
-    type Span = { start: int; stop: int }
+  type Span = { start: int; stop: int }
 
-    type DeclKind =
-        | TypeDecl of
-            name: string *
-            type_ann: TypeAnn *
-            type_params: option<list<TypeParam>>
-        | VarDecl of
-            pattern: Pattern *
-            init: option<Expr> *
-            type_ann: option<TypeAnn> *
-            is_declare: bool
+  type DeclKind =
+    | TypeDecl of
+      name: string *
+      type_ann: TypeAnn *
+      type_params: option<list<TypeParam>>
+    | VarDecl of
+      pattern: Pattern *
+      init: option<Expr> *
+      type_ann: option<TypeAnn> *
+      is_declare: bool
 
-    type Decl = { span: Span; kind: DeclKind }
+  type Decl = { span: Span; kind: DeclKind }
 
-    type StmtKind =
-        | Expr of Expr
-        | For of leff: Pattern * right: Expr * body: Block
-        | Return of option<Expr>
-        | Decl of Decl
+  type StmtKind =
+    | Expr of Expr
+    | For of leff: Pattern * right: Expr * body: Block
+    | Return of option<Expr>
+    | Decl of Decl
 
-    type Stmt = { span: Span; kind: StmtKind }
+  type Stmt = { span: Span; kind: StmtKind }
 
-    type Literal =
-        | Number of string
-        | String of string
-        | Boolean of bool
-        | Null
-        | Undefined
+  type Literal =
+    | Number of string
+    | String of string
+    | Boolean of bool
+    | Null
+    | Undefined
 
-    type ObjPatElem =
-        | KeyValue of
-            span: Span *
-            key: string *
-            value: Pattern *
-            init: option<Expr>
-        | Shorthand of
-            span: Span *
-            name: string *
-            init: option<Expr> *
-            is_mut: bool
+  type ObjPatElem =
+    | KeyValue of span: Span * key: string * value: Pattern * init: option<Expr>
+    | Shorthand of span: Span * name: string * init: option<Expr> * is_mut: bool
 
-    [<RequireQualifiedAccess>]
-    type PatternKind =
-        | Identifier of span: Span * name: string * is_mut: bool
-        | Object of elems: list<ObjElem>
-        | Tuple of elems: list<Pattern>
-        | Wildcard of span: Span
-        | Literal of span: Span * value: Literal
-        | Is of span: Span * target: Pattern * id: string * is_mut: bool
+  [<RequireQualifiedAccess>]
+  type PatternKind =
+    | Identifier of span: Span * name: string * is_mut: bool
+    | Object of elems: list<ObjElem>
+    | Tuple of elems: list<Pattern>
+    | Wildcard of span: Span
+    | Literal of span: Span * value: Literal
+    | Is of span: Span * target: Pattern * id: string * is_mut: bool
 
-    type Pattern =
-        { kind: PatternKind
-          span: Span
-          inferred_type: option<Type.Type> }
+  type Pattern =
+    { kind: PatternKind
+      span: Span
+      inferred_type: option<Type.Type> }
 
-    type Block = { span: Span; stmts: list<Stmt> }
+  type Block = { span: Span; stmts: list<Stmt> }
 
-    [<RequireQualifiedAccess>]
-    type BlockOrExpr =
-        | Block of Block
-        | Expr of Expr
+  [<RequireQualifiedAccess>]
+  type BlockOrExpr =
+    | Block of Block
+    | Expr of Expr
 
-    type ObjElem =
-        | Property of span: Span * key: string * value: Expr
-        | Spread of span: Span * value: Expr
+  type ObjElem =
+    | Property of span: Span * key: string * value: Expr
+    | Spread of span: Span * value: Expr
 
-    type BinaryOp =
-        | Add
-        | Subtract
-        | Multiply
-        | Divide
-        | Modulo
-        | Equal
-        | NotEqual
-        | LessThan
-        | LessThanOrEqual
-        | GreaterThan
-        | GreaterThanOrEqual
-        | And
-        | Or
+  type AssignOp =
+    | Assign
+    | AddAssign
+    | SubAssign
+    | MulAssign
+    | DivAssign
+    | ModAssign
+    | ExpAssign
 
-    type UnaryOp =
-        | Pos
-        | Neg
-        | Not
+  type BinaryOp =
+    | Add
+    | Sub
+    | Mul
+    | Div
+    | Mod
+    | Exp
+    | Equal
+    | NotEqual
+    | LessThan
+    | LessThanOrEqual
+    | GreaterThan
+    | GreaterThanOrEqual
+    | And
+    | Or
 
-    type MatchCase =
-        { span: Span
-          pattern: Pattern
-          guard: option<Expr>
-          body: Expr }
+  type UnaryOp =
+    | Pos
+    | Neg
+    | Not
 
-    type TemplateLiteral =
-        { parts: list<Expr>; exprs: list<Expr> }
+  type MatchCase =
+    { span: Span
+      pattern: Pattern
+      guard: option<Expr>
+      body: Expr }
 
-    type ExprKind =
-        | Identifer of span: Span * name: string
-        | Literal of Literal
-        | Object of elems: list<ObjElem>
-        | Tuple of elems: list<Expr>
-        | Binary of left: Expr * op: BinaryOp * right: Expr
-        | Unary of op: string * value: Expr
-        | Function of param_list: list<string> * body: BlockOrExpr
-        | Call of
-            callee: Expr *
-            type_args: option<list<TypeAnn>> *
-            args: list<Expr> *
-            opt_chain: bool *
-            throws: option<Type.Type>
-        | Index of target: Expr * index: Expr * opt_chain: bool
-        | Member of target: Expr * name: string * opt_chain: bool
-        | If of cond: Expr * then_branch: Expr * else_branch: Expr
-        | Match of target: Expr * cases: list<MatchCase>
-        | Try of body: Block * catch: option<Expr> * finally_: option<Expr>
-        | Do of body: Block
-        | Await of value: Expr
-        | Throw of value: Expr
-        | TemplateLiteral of TemplateLiteral
-        | TaggedTemplateLiteral of
-            tag: Expr *
-            template: TemplateLiteral *
-            throws: option<Type.Type>
+  type TemplateLiteral =
+    { parts: list<string>
+      exprs: list<Expr> }
 
-    type Expr =
-        { kind: ExprKind
-          span: Span
-          inferred_type: option<Type.Type> ref }
+  type ExprKind =
+    | Identifer of string
+    | Literal of Literal
+    | Object of elems: list<ObjElem>
+    | Tuple of elems: list<Expr>
+    | Assign of left: Expr * op: AssignOp * right: Expr
+    | Binary of left: Expr * op: BinaryOp * right: Expr
+    | Unary of op: string * value: Expr
+    | Function of param_list: list<string> * body: BlockOrExpr
+    | Call of
+      callee: Expr *
+      type_args: option<list<TypeAnn>> *
+      args: list<Expr> *
+      opt_chain: bool *
+      throws: option<Type.Type>
+    | Index of target: Expr * index: Expr * opt_chain: bool
+    | Member of target: Expr * name: string * opt_chain: bool
+    | If of cond: Expr * then_branch: Expr * else_branch: Expr
+    | Match of target: Expr * cases: list<MatchCase>
+    | Try of body: Block * catch: option<Expr> * finally_: option<Expr>
+    | Do of body: Block
+    | Await of value: Expr
+    | Throw of value: Expr
+    | TemplateLiteral of TemplateLiteral
+    | TaggedTemplateLiteral of
+      tag: Expr *
+      template: TemplateLiteral *
+      throws: option<Type.Type>
 
-    type ObjTypeAnnElem = int
+  type Expr =
+    { kind: ExprKind
+      span: Span
+      inferred_type: option<Type.Type> ref }
 
-    type KeywordType =
-        | Boolean
-        | Number
-        | String
-        | Symbol
-        | Null
-        | Undefined
-        | Unknown
-        | Never
+  type ObjTypeAnnElem = int
 
-    type TypeParam =
-        { span: Span
-          name: string
-          bound: option<Type.Type>
-          default_: option<Type.Type> }
+  type KeywordType =
+    | Boolean
+    | Number
+    | String
+    | Symbol
+    | Null
+    | Undefined
+    | Unknown
+    | Never
 
-    type FunctionType =
-        { type_params: option<list<TypeParam>>
-          params_: list<TypeAnn>
-          return_type: TypeAnn
-          throws: option<Type.Type> }
+  type TypeParam =
+    { span: Span
+      name: string
+      bound: option<Type.Type>
+      default_: option<Type.Type> }
 
-    type ConditionType =
-        { check: TypeAnn
-          extends: TypeAnn
-          true_type: TypeAnn
-          false_type: TypeAnn }
+  type FunctionType =
+    { type_params: option<list<TypeParam>>
+      params_: list<TypeAnn>
+      return_type: TypeAnn
+      throws: option<Type.Type> }
 
-    type MatchType =
-        { target: TypeAnn
-          cases: list<MatchTypeCase> }
+  type ConditionType =
+    { check: TypeAnn
+      extends: TypeAnn
+      true_type: TypeAnn
+      false_type: TypeAnn }
 
-    type MatchTypeCase =
-        { extends: TypeAnn; true_type: TypeAnn }
+  type MatchType =
+    { target: TypeAnn
+      cases: list<MatchTypeCase> }
 
-    type Ident = Span * string
+  type MatchTypeCase =
+    { extends: TypeAnn; true_type: TypeAnn }
 
-    type TypeAnnKind =
-        | BooleanLiteral of value: bool
-        | NumberLiteral of value: string
-        | StringLiteral of value: string
-        | Keyword of keyword: KeywordType
-        | Object of elems: list<ObjTypeAnnElem>
-        | Tuple of elems: list<TypeAnn>
-        | Array of elem: TypeAnn
-        | Union of types: list<TypeAnn>
-        | Intersection of types: list<TypeAnn>
-        | TypeRef of name: string * type_args: list<TypeAnn>
-        | Function of FunctionType
-        | KeyOf of target: TypeAnn
-        | Rest of target: TypeAnn
-        | TypeOf of target: Ident
-        | Index of target: TypeAnn * index: TypeAnn
-        | Condition of ConditionType
-        | Match of MatchType
-        | Infer of name: string
-        | Wildcard
-        | Binary of left: TypeAnn * op: BinaryOp * right: TypeAnn
+  type Ident = Span * string
 
-    type TypeAnn =
-        { kind: TypeAnnKind
-          span: Span
-          inferred_type: option<Type.Type> ref }
+  type TypeAnnKind =
+    | BooleanLiteral of value: bool
+    | NumberLiteral of value: string
+    | StringLiteral of value: string
+    | Keyword of keyword: KeywordType
+    | Object of elems: list<ObjTypeAnnElem>
+    | Tuple of elems: list<TypeAnn>
+    | Array of elem: TypeAnn
+    | Union of types: list<TypeAnn>
+    | Intersection of types: list<TypeAnn>
+    | TypeRef of name: string * type_args: list<TypeAnn>
+    | Function of FunctionType
+    | KeyOf of target: TypeAnn
+    | Rest of target: TypeAnn
+    | TypeOf of target: Ident
+    | Index of target: TypeAnn * index: TypeAnn
+    | Condition of ConditionType
+    | Match of MatchType
+    | Infer of name: string
+    | Wildcard
+    | Binary of left: TypeAnn * op: BinaryOp * right: TypeAnn
+
+  type TypeAnn =
+    { kind: TypeAnnKind
+      span: Span
+      inferred_type: option<Type.Type> ref }
 
 module Type =
-    type TypeParam =
-        { name: string
-          bound: option<Type>
-          default_: option<Type> }
+  type TypeParam =
+    { name: string
+      bound: option<Type>
+      default_: option<Type> }
 
-    type Scheme =
-        { type_params: list<TypeParam>
-          type_: Type
-          is_type_param: bool }
+  type Scheme =
+    { type_params: list<TypeParam>
+      type_: Type
+      is_type_param: bool }
 
-    type Pattern =
-        | Identifier of name: string
-        | Object of elems: list<ObjPatElem>
-        | Tuple of elems: list<Pattern>
-        | Wildcard
-        | Literal of Syntax.Literal
-        | Is of target: Pattern * id: string
-        | Rest of target: Pattern
+  type Pattern =
+    | Identifier of name: string
+    | Object of elems: list<ObjPatElem>
+    | Tuple of elems: list<Pattern>
+    | Wildcard
+    | Literal of Syntax.Literal
+    | Is of target: Pattern * id: string
+    | Rest of target: Pattern
 
-    type ObjPatElem =
-        | KeyValue of key: string * value: Pattern
-        | Shorthand of name: string * value: option<Type>
-        | Rest of target: Pattern
+  type ObjPatElem =
+    | KeyValue of key: string * value: Pattern
+    | Shorthand of name: string * value: option<Type>
+    | Rest of target: Pattern
 
-    type FuncParam =
-        { pattern: Pattern
-          type_: Type
-          optional: bool }
+  type FuncParam =
+    { pattern: Pattern
+      type_: Type
+      optional: bool }
 
-    [<RequireQualifiedAccess>]
-    type KeywordType =
-        | Never
-        | Object
-        | Unknown
+  [<RequireQualifiedAccess>]
+  type KeywordType =
+    | Never
+    | Object
+    | Unknown
 
-    type Primitive =
-        | Boolean
-        | Number
-        | String
-        | Symbol
+  type Primitive =
+    | Boolean
+    | Number
+    | String
+    | Symbol
 
-    type Function =
-        { param_list: list<FuncParam>
-          return_type: Type
-          type_params: option<list<TypeParam>>
-          throws: Type }
+  type Function =
+    { param_list: list<FuncParam>
+      return_type: Type
+      type_params: option<list<TypeParam>>
+      throws: Type }
 
-    type Mapped =
-        { key: Type
-          value: Type
-          target: string
-          source: Type
-          optional: option<MappedModifier>
+  type Mapped =
+    { key: Type
+      value: Type
+      target: string
+      source: Type
+      optional: option<MappedModifier>
 
-          // First half of Conditional
-          check: option<Type>
-          extends: option<Type> }
+      // First half of Conditional
+      check: option<Type>
+      extends: option<Type> }
 
-    type MappedModifier =
-        | Add
-        | Remove
+  type MappedModifier =
+    | Add
+    | Remove
 
-    type ObjKey = string // TODO
+  type ObjKey = string // TODO
 
-    type ObjTypeElem =
-        | Call of Function
-        | Constructor of Function
-        | Method of name: ObjKey * is_mut: bool * type_: Function
-        | Getter of name: ObjKey * return_type: Type * throws: Type
-        | Setter of name: ObjKey * param: FuncParam * throws: Type
-        | Mapped of Mapped
-        | Property of
-            name: ObjKey *
-            optional: bool *
-            readonly: bool *
-            type_: Type
+  type ObjTypeElem =
+    | Call of Function
+    | Constructor of Function
+    | Method of name: ObjKey * is_mut: bool * type_: Function
+    | Getter of name: ObjKey * return_type: Type * throws: Type
+    | Setter of name: ObjKey * param: FuncParam * throws: Type
+    | Mapped of Mapped
+    | Property of name: ObjKey * optional: bool * readonly: bool * type_: Type
 
-    [<RequireQualifiedAccess>]
-    type TypeKind =
-        | TypeVar of id: int * instance: option<Type> * bound: option<Type>
-        | TypeRef of
-            name: string *
-            type_args: list<Type> *
-            scheme: option<Scheme>
-        | Literal of Syntax.Literal
-        | Tuple of elems: list<Type>
-        | Array of elem: Type
-        | Union of types: list<Type>
-        | Intersection of types: list<Type>
-        | Keyword of keyword: KeywordType
-        | Function of Function
-        | Object of elems: list<ObjTypeElem>
-        | Rest of target: Type
-        | KeyOf of target: Type
-        | Index of target: Type * index: Type
-        | Condition of
-            check: Type *
-            extends: Type *
-            true_type: Type *
-            false_type: Type
-        | Infer of name: string
-        | Wildcard
-        | Binary of left: Type * op: Syntax.BinaryOp * right: Type
+  [<RequireQualifiedAccess>]
+  type TypeKind =
+    | TypeVar of id: int * instance: option<Type> * bound: option<Type>
+    | TypeRef of name: string * type_args: list<Type> * scheme: option<Scheme>
+    | Literal of Syntax.Literal
+    | Tuple of elems: list<Type>
+    | Array of elem: Type
+    | Union of types: list<Type>
+    | Intersection of types: list<Type>
+    | Keyword of keyword: KeywordType
+    | Function of Function
+    | Object of elems: list<ObjTypeElem>
+    | Rest of target: Type
+    | KeyOf of target: Type
+    | Index of target: Type * index: Type
+    | Condition of
+      check: Type *
+      extends: Type *
+      true_type: Type *
+      false_type: Type
+    | Infer of name: string
+    | Wildcard
+    | Binary of left: Type * op: Syntax.BinaryOp * right: Type
 
-    [<RequireQualifiedAccess>]
-    type Provenance =
-        | Type of Type
-        | Expr of Syntax.Expr
+  [<RequireQualifiedAccess>]
+  type Provenance =
+    | Type of Type
+    | Expr of Syntax.Expr
 
-    type Type =
-        { kind: TypeKind
-          provenance: option<Provenance> ref }
+  type Type =
+    { kind: TypeKind
+      provenance: option<Provenance> ref }
--- src/Escalier.Parser/ExprParser.fs
+++ src/Escalier.Parser/ExprParser.fs
@@ -4,68 +4,353 @@ open FParsec
 open Escalier.Data.Syntax
 
 module ExprParser =
-    let ws = spaces
+  let mergeSpans (x: Span) (y: Span) = { start = x.start; stop = y.stop }
 
-    let str_ws s = pstring s .>> ws
+  let opp = new OperatorPrecedenceParser<Expr, unit, unit>()
+  let expr: Parser<Expr, unit> = opp.ExpressionParser
 
-    let number: Parser<Expr, unit> =
-        pipe3 getPosition pfloat getPosition
-        <| fun p1 nl p2 ->
-            let start = p1.Index |> int
-            let stop = p2.Index |> int
+  let ws = spaces
 
-            { kind = ExprKind.Literal(Literal.Number(nl |> string))
+  let str_ws s = pstring s .>> ws
+
+  let number: Parser<Expr, unit> =
+    pipe3 getPosition pfloat getPosition
+    <| fun p1 nl p2 ->
+      let start = p1.Index |> int
+      let stop = p2.Index |> int
+
+      { kind = Literal(Literal.Number(nl |> string))
+        span = { start = start; stop = stop }
+        inferred_type = ref None }
+
+  let identifier: Parser<Expr, unit> =
+    let isIdentifierFirstChar c = isLetter c || c = '_'
+    let isIdentifierChar c = isLetter c || isDigit c || c = '_'
+
+    let _ident =
+      many1Satisfy2L isIdentifierFirstChar isIdentifierChar "identifier" .>> ws // skips trailing whitespace
+
+    pipe3 getPosition _ident getPosition
+    <| fun p1 sl p2 ->
+      let start = p1.Index |> int
+      let stop = p2.Index |> int
+
+      { kind = Identifer(sl)
+        span = { start = start; stop = stop }
+        inferred_type = ref None }
+
+  let stringLiteral: Parser<Expr, unit> =
+    let normalCharSnippet = manySatisfy (fun c -> c <> '\\' && c <> '"')
+
+    let unescape c =
+      match c with
+      | 'n' -> "\n"
+      | 'r' -> "\r"
+      | 't' -> "\t"
+      | c -> string c
+
+    let escapedChar = pstring "\\" >>. (anyOf "\\nrt\"" |>> unescape)
+
+    let _string =
+      (between
+        (pstring "\"")
+        (pstring "\"")
+        (stringsSepBy normalCharSnippet escapedChar))
+
+    pipe3 getPosition _string getPosition
+    <| fun p1 sl p2 ->
+      let start = p1.Index |> int
+      let stop = p2.Index |> int
+
+      { kind = ExprKind.Literal(Literal.String(sl))
+        span = { start = start; stop = stop }
+        inferred_type = ref None }
+
+
+  let templateStringLiteral: Parser<Expr, unit> =
+    fun stream ->
+      let mutable currentString = ""
+      let mutable parts: list<string> = []
+      let mutable exprs: list<Expr> = []
+      let mutable reply: option<Reply<Expr>> = None
+
+      if stream.Peek() = '`' then
+        let start = stream.Index |> int
+        stream.Skip() // '`'
+
+        while stream.Peek() <> '`' && reply = None do
+          if stream.PeekString(2) = "${" then
+            stream.Skip(2) // '${'
+            parts <- currentString :: parts
+            currentString <- ""
+            let e = expr stream
+
+            if e.Status = ReplyStatus.Ok then
+              if stream.Peek() = '}' then
+                stream.Skip()
+                exprs <- e.Result :: exprs
+              else
+                reply <- Some(Reply(Error, messageError "Expected '}'"))
+            else
+              reply <- Some(e)
+          else
+            currentString <- currentString + string (stream.Read())
+
+        match reply with
+        | None ->
+          stream.Skip() // '`'
+          let stop = stream.Index |> int
+          parts <- currentString :: parts
+
+          let result: Expr =
+            { kind =
+                TemplateLiteral(
+                  { parts = List.rev parts
+                    exprs = List.rev exprs }
+                )
               span = { start = start; stop = stop }
               inferred_type = ref None }
 
-    let opp = new OperatorPrecedenceParser<Expr, unit, unit>()
-    let expr = opp.ExpressionParser
-    let term = (number .>> ws) <|> between (str_ws "(") (str_ws ")") expr
-    opp.TermParser <- term
+          Reply(result)
+        | Some(value) -> value
+      else
+        Reply(Error, messageError "Expected '`'")
 
-    type Assoc = Associativity
+  let atom = number <|> identifier <|> stringLiteral <|> templateStringLiteral
+  let term = (atom .>> ws) <|> between (str_ws "(") (str_ws ")") expr
 
-    opp.AddOperator(
-        InfixOperator(
-            "+",
-            ws,
-            1,
-            Assoc.Left,
-            (fun x y ->
-                { Expr.kind = ExprKind.Binary(x, BinaryOp.Add, y)
-                  span =
-                    { start = x.span.start
-                      stop = y.span.stop }
-                  inferred_type = ref None })
-        )
+  opp.TermParser <- term
+
+  type Assoc = Associativity
+
+  // logical not (14)
+  // bitwise not (14)
+
+  opp.AddOperator(
+    PrefixOperator(
+      "+",
+      ws,
+      14,
+      true,
+      (fun x ->
+        { Expr.kind = Unary("+", x)
+          span = x.span
+          inferred_type = ref None })
     )
+  )
 
-    opp.AddOperator(
-        InfixOperator(
-            "*",
-            ws,
-            2,
-            Assoc.Left,
-            (fun x y ->
-                { Expr.kind = ExprKind.Binary(x, BinaryOp.Add, y)
-                  span =
-                    { start = x.span.start
-                      stop = y.span.stop }
-                  inferred_type = ref None })
-        )
+  opp.AddOperator(
+    PrefixOperator(
+      "-",
+      ws,
+      14,
+      true,
+      (fun x ->
+        { Expr.kind = Unary("-", x)
+          span = x.span
+          inferred_type = ref None })
     )
+  )
 
-    opp.AddOperator(
-        PrefixOperator(
-            "-",
-            ws,
-            3,
-            true,
-            (fun x ->
-                { Expr.kind = ExprKind.Unary("-", x)
-                  span =
-                    { start = x.span.start
-                      stop = x.span.stop }
-                  inferred_type = ref None })
-        )
+  // typeof (14)
+  // delete (14)
+  // await (14)
+
+  opp.AddOperator(
+    InfixOperator(
+      "**",
+      ws,
+      13,
+      Assoc.Right,
+      (fun x y ->
+        { Expr.kind = ExprKind.Binary(x, Exp, y)
+          span = mergeSpans x.span y.span
+          inferred_type = ref None })
     )
+  )
+
+  opp.AddOperator(
+    InfixOperator(
+      "*",
+      ws,
+      12,
+      Assoc.Left,
+      (fun x y ->
+        { Expr.kind = ExprKind.Binary(x, Mul, y)
+          span = mergeSpans x.span y.span
+          inferred_type = ref None })
+    )
+  )
+
+  opp.AddOperator(
+    InfixOperator(
+      "/",
+      ws,
+      12,
+      Assoc.Left,
+      (fun x y ->
+        { Expr.kind = ExprKind.Binary(x, Div, y)
+          span = mergeSpans x.span y.span
+          inferred_type = ref None })
+    )
+  )
+
+  opp.AddOperator(
+    InfixOperator(
+      "%",
+      ws,
+      12,
+      Assoc.Left,
+      (fun x y ->
+        { Expr.kind = ExprKind.Binary(x, Mod, y)
+          span = mergeSpans x.span y.span
+          inferred_type = ref None })
+    )
+  )
+
+  opp.AddOperator(
+    InfixOperator(
+      "+",
+      ws,
+      11,
+      Assoc.Left,
+      (fun x y ->
+        { Expr.kind = ExprKind.Binary(x, Add, y)
+          span = mergeSpans x.span y.span
+          inferred_type = ref None })
+    )
+  )
+
+  opp.AddOperator(
+    InfixOperator(
+      "-",
+      ws,
+      11,
+      Assoc.Left,
+      (fun x y ->
+        { Expr.kind = ExprKind.Binary(x, Sub, y)
+          span = mergeSpans x.span y.span
+          inferred_type = ref None })
+    )
+  )
+
+  opp.AddOperator(
+    InfixOperator(
+      "<",
+      ws,
+      9,
+      Assoc.Left,
+      (fun x y ->
+        { Expr.kind = ExprKind.Binary(x, LessThan, y)
+          span = mergeSpans x.span y.span
+          inferred_type = ref None })
+    )
+  )
+
+  opp.AddOperator(
+    InfixOperator(
+      "<=",
+      ws,
+      9,
+      Assoc.Left,
+      (fun x y ->
+        { Expr.kind = ExprKind.Binary(x, LessThanOrEqual, y)
+          span = mergeSpans x.span y.span
+          inferred_type = ref None })
+    )
+  )
+
+  opp.AddOperator(
+    InfixOperator(
+      ">",
+      ws,
+      9,
+      Assoc.Left,
+      (fun x y ->
+        { Expr.kind = ExprKind.Binary(x, GreaterThan, y)
+          span = mergeSpans x.span y.span
+          inferred_type = ref None })
+    )
+  )
+
+  opp.AddOperator(
+    InfixOperator(
+      ">=",
+      ws,
+      9,
+      Assoc.Left,
+      (fun x y ->
+        { Expr.kind = ExprKind.Binary(x, GreaterThanOrEqual, y)
+          span = mergeSpans x.span y.span
+          inferred_type = ref None })
+    )
+  )
+
+  opp.AddOperator(
+    InfixOperator(
+      "==",
+      ws,
+      8,
+      Assoc.Left,
+      (fun x y ->
+        { Expr.kind = ExprKind.Binary(x, Equal, y)
+          span = mergeSpans x.span y.span
+          inferred_type = ref None })
+    )
+  )
+
+  opp.AddOperator(
+    InfixOperator(
+      "!=",
+      ws,
+      8,
+      Assoc.Left,
+      (fun x y ->
+        { Expr.kind = ExprKind.Binary(x, NotEqual, y)
+          span = mergeSpans x.span y.span
+          inferred_type = ref None })
+    )
+  )
+
+  // bitwise and (7)
+  // bitwise xor (6)
+  // bitwise or (5)
+
+  opp.AddOperator(
+    InfixOperator(
+      "&&",
+      ws,
+      4,
+      Assoc.Left,
+      (fun x y ->
+        { Expr.kind = ExprKind.Binary(x, And, y)
+          span = mergeSpans x.span y.span
+          inferred_type = ref None })
+    )
+  )
+
+  opp.AddOperator(
+    InfixOperator(
+      "||",
+      ws,
+      3,
+      Assoc.Left,
+      (fun x y ->
+        { Expr.kind = ExprKind.Binary(x, Or, y)
+          span = mergeSpans x.span y.span
+          inferred_type = ref None })
+    )
+  )
+
+  opp.AddOperator(
+    InfixOperator(
+      "=",
+      ws,
+      2,
+      Assoc.Right,
+      (fun x y ->
+        { Expr.kind = ExprKind.Assign(x, AssignOp.Assign, y)
+          span = mergeSpans x.span y.span
+          inferred_type = ref None })
+    )
+  )

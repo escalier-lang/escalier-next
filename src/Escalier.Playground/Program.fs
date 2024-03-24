f9be1ac4a01891086afeaa29bb2203d80f858fa1
Kevin Barabash <kevinb7@gmail.com>
2024-03-24 16:49:45 -0400

    add compileString to Escalier.Playground and update playground to call it with source code from a test program

--- src/Escalier.Playground/Program.fs
+++ src/Escalier.Playground/Program.fs
@@ -1,7 +1,9 @@
-﻿open System.IO
-open System.Text.Json
+﻿open FSharp.Data
+open System.IO
 open System.Runtime.InteropServices
 open Extism
+
+open Escalier.Compiler
 
 type Data = { greeting: string; name: string }
 
@@ -14,12 +16,32 @@ let Greet () : int32 =
 
   let input = Pdk.GetInputString()
   printfn $"input = {input}"
-  let data = JsonSerializer.Deserialize<Data> input
+  let data = JsonValue.Parse input
+  let greeting = data.GetProperty "greeting" |> _.AsString()
+  let name = data.GetProperty "name" |> _.AsString()
 
-  printfn $"greeting = {data.greeting}, name = {data.name}"
+  printfn $"greeting = {greeting}, name = {name}"
 
-  let result = $"{data.greeting}, {data.name}!"
+  let result = $"{greeting}, {name}!"
   Pdk.SetOutput(result)
+  0
+
+[<UnmanagedCallersOnly(EntryPoint = "compile")>]
+let Compile () : int32 =
+  printfn "Compile called"
+
+  let srcCode = Pdk.GetInputString() // filename
+  let textWriter = new StringWriter()
+
+  match Compiler.compileString textWriter "/" srcCode with
+  | Ok(js, dts) ->
+    let js = System.Web.HttpUtility.JavaScriptStringEncode js
+    let dts = System.Web.HttpUtility.JavaScriptStringEncode dts
+    Pdk.SetOutput($"{{\"js\": \"{js}\", \"dts\": \"{dts}\"}}")
+  | Error errorValue ->
+    // TODO: update CompileOutput to include an error field
+    printfn $"errorValue = {errorValue}"
+
   0
 
 [<EntryPoint>]

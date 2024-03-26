9ff3413c5a8ea77bf3f71c153f6dce196fe1c744
Kevin Barabash <kevinb7@gmail.com>
2024-03-25 21:20:57 -0400

    fix the prod base name in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: process.env.APP_ENV === "prod" ? "/esalier-next/" : "/",
+    base: process.env.APP_ENV === "prod" ? "/escalier-next/" : "/",
 })

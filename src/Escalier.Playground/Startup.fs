578e618c5b6a3336eab1b284d65268b7eb5421a7
Kevin Barabash <kevinb7@gmail.com>
2024-12-04 23:01:03 -0500

    Create new Bolero project that will eventually replace the current playground (#378)
    * remove playground
    
    * create new Bolero project
    
    * add build script
    
    * make build.sh executable, update pr.yml's 'build' step
    
    * try cleaning before building
    
    * see if installing bolero templates helps
    
    * update version of boloero.build
    
    * pin the version of dotnet
    
    * print out the version of .NET
    
    * try changing the root namespace
    
    * try a more recent version of the 'setup-dotnet' action
    
    * pin the .NET version in global.json

---
+++ src/Escalier.Playground/Startup.fs
@@ -0,0 +1,20 @@
+namespace Escalier.Playground
+
+open Microsoft.AspNetCore.Components.WebAssembly.Hosting
+open Microsoft.Extensions.DependencyInjection
+open System
+open System.Net.Http
+
+module Program =
+
+  [<EntryPoint>]
+  let Main args =
+    let builder = WebAssemblyHostBuilder.CreateDefault(args)
+    builder.RootComponents.Add<Main.MyApp>("#main")
+
+    builder.Services.AddScoped<HttpClient>(fun _ ->
+      new HttpClient(BaseAddress = Uri builder.HostEnvironment.BaseAddress))
+    |> ignore
+
+    builder.Build().RunAsync() |> ignore
+    0

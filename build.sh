274298100d5cd779d77a55e55c6b39546e189545
Kevin Barabash <kevinb7@gmail.com>
2024-12-04 23:24:43 -0500

    fix build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ pushd out || exit 1
   mv wwwroot escalier-next
 popd || exit 1
 if [ ! -e "out/escalier-next/node_modules" ]; then
-  pushd out/wwwroot || exit 1
+  pushd out/escalier-next || exit 1
     ln -s ../../node_modules node_modules
   popd || exit 1
 fi
